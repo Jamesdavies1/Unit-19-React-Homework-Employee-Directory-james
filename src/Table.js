50e00dc37e5fca6ee393dbdfcf10a09e90da2ea7
james davies <jamesdavies@jamess-imac.home>
2020-04-30 21:26:56 +0100

    Added sort to my first name

--- src/Table.js
+++ src/Table.js
@@ -1,57 +1,85 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Table = ({ users }) => {
-  return (
-    <table class="table">
-      <thead>
-        <tr>
-          <th scope="col">title</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">gender</th>
-          <th scope="col">email</th>
-          <th scope="col">phone</th>
-          <th scope="col">cell</th>
-          <th scope="col">city</th>
-          <th scope="col">state</th>
-          <th scope="col">country</th>
-          <th scope="col">postcode</th>
-          <th scope="col">picture</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map(
-          ({
-            location: { city, state, country, postcode },
-            picture: { thumbnail },
-            cell,
-            phone,
-            gender,
-            email,
-            name: { first, last, title }
-          }) => (
-            <tr>
-              <td>{title}</td>
-              <th>{first}</th>
-              <td>{last}</td>
-              <td>{gender}</td>
-              <td>{email}</td>
-              <td>{phone}</td>
-              <td>{cell}</td>
-              <td>{city}</td>
-              <td>{state}</td>
-              <td>{country}</td>
-              <td>{postcode}</td>
-              <td>
-                <img src={thumbnail} />
-              </td>
+  const [sortedUsers, updateSortedUsers] = useState([]);
 
-              <td></td>
-            </tr>
-          )
-        )}
-      </tbody>
-    </table>
+  useEffect(() => updateSortedUsers(users), [users]);
+
+  return (
+    <div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">title</th>
+            <th
+              scope="col"
+              onClick={() => {
+                const usersCopy = [...users];
+                const updateSort = usersCopy.sort((a, b) => {
+                  const nameA = a.name.first;
+                  const nameB = b.name.first;
+
+                  if (nameA < nameB) {
+                    return -1;
+                  }
+                  if (nameA > nameB) {
+                    return 1;
+                  }
+
+                  return 0;
+                });
+
+                updateSortedUsers(updateSort);
+              }}
+            >
+              First
+            </th>
+            <th scope="col">Last</th>
+            <th scope="col">gender</th>
+            <th scope="col">email</th>
+            <th scope="col">phone</th>
+            <th scope="col">cell</th>
+            <th scope="col">city</th>
+            <th scope="col">state</th>
+            <th scope="col">country</th>
+            <th scope="col">postcode</th>
+            <th scope="col">picture</th>
+          </tr>
+        </thead>
+        <tbody>
+          {sortedUsers.map(
+            ({
+              location: { city, state, country, postcode },
+              picture: { thumbnail },
+              cell,
+              phone,
+              gender,
+              email,
+              name: { first, last, title }
+            }) => (
+              <tr key={email}>
+                <td>{title}</td>
+                <th>{first}</th>
+                <td>{last}</td>
+                <td>{gender}</td>
+                <td>{email}</td>
+                <td>{phone}</td>
+                <td>{cell}</td>
+                <td>{city}</td>
+                <td>{state}</td>
+                <td>{country}</td>
+                <td>{postcode}</td>
+                <td>
+                  <img src={thumbnail} />
+                </td>
+
+                <td></td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
