b4e22a467521c7dc7dd819c6750bf31ccb832093
james davies <jamesdavies@jamess-imac.home>
2020-04-30 19:41:37 +0100

    added new cols inc images, address, phone numbers and more

--- src/Table.js
+++ src/Table.js
@@ -8,16 +8,48 @@ const Table = ({ users }) => {
           <th scope="col">title</th>
           <th scope="col">First</th>
           <th scope="col">Last</th>
+          <th scope="col">gender</th>
+          <th scope="col">email</th>
+          <th scope="col">phone</th>
+          <th scope="col">cell</th>
+          <th scope="col">city</th>
+          <th scope="col">state</th>
+          <th scope="col">country</th>
+          <th scope="col">postcode</th>
+          <th scope="col">picture</th>
         </tr>
       </thead>
       <tbody>
-        {users.map(({ name: { first, last, title } }) => (
-          <tr>
-            <td>{title}</td>
-            <th>{first}</th>
-            <td>{last}</td>
-          </tr>
-        ))}
+        {users.map(
+          ({
+            location: { city, state, country, postcode },
+            picture: { thumbnail },
+            cell,
+            phone,
+            gender,
+            email,
+            name: { first, last, title }
+          }) => (
+            <tr>
+              <td>{title}</td>
+              <th>{first}</th>
+              <td>{last}</td>
+              <td>{gender}</td>
+              <td>{email}</td>
+              <td>{phone}</td>
+              <td>{cell}</td>
+              <td>{city}</td>
+              <td>{state}</td>
+              <td>{country}</td>
+              <td>{postcode}</td>
+              <td>
+                <img src={thumbnail} />
+              </td>
+
+              <td></td>
+            </tr>
+          )
+        )}
       </tbody>
     </table>
   );
